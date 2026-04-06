41ac8f07d6d4746d196e1649253cd7170db5cbe5
Yash Raj <yashraj4586@gmail.com>
2026-04-07 00:19:17 +0530

    Swap of two variables

---
+++ swap-two-nums.js
@@ -0,0 +1,55 @@
+//using temp variable
+
+// function swapTwoNums(a, b) {
+//     let temp=a;
+//     a=b;
+//     b=temp;
+
+//     return [a,b]
+// }
+
+// console.log(swapTwoNums(2,5))
+
+//using destructuring
+
+// function swapTwoNums(a,b){
+//     return [a,b] = [b,a]
+// }
+
+// console.log(swapTwoNums(2,5))
+
+// using addn and subn
+
+// function swapTwoNums(a,b){
+//     a=a+b;
+//     b=a-b;
+//     a=a-b;
+
+//     return [a,b]
+// }
+
+// console.log(swapTwoNums(2,5))
+
+// using mul and div
+
+// function swapTwoNums(a,b){
+//     a=a*b;
+//     b=a/b;
+//     a=a/b;
+
+//     return [a,b]
+// }
+
+// console.log(swapTwoNums(2,5))
+
+// using axor operation
+
+function swapTwoNums(a, b) {
+  a = a ^ b;
+  b = a ^ b;
+  a = a ^ b;
+
+  return [a, b];
+}
+
+console.log(swapTwoNums(2,5))
