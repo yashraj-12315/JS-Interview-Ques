80ff615e4bc2000747930d87a73be131fb92e4c6
Yash Raj <yashraj4586@gmail.com>
2026-04-06 23:51:04 +0530

    Real world sensitive info hidding using proxy

--- proxy.js
+++ proxy.js
@@ -1,27 +1,48 @@
-const p1 = {
-  fname: "Yash",
-  lname: "Raj",
-  age: 18,
+// const p1 = {
+//   fname: "Yash",
+//   lname: "Raj",
+//   age: 18,
+// };
+
+// const p1Proxy = new Proxy(p1, {
+//   get(target, prop) {
+//     if(prop in target) {
+//       return target[prop];
+//     } else {
+//       return "Property does not exist";
+//     }
+//   },
+//   set(target, prop, value) {
+//     if(prop === "age" && value < 0) {
+//       console.log("Age cannot be negative");
+//       return false; // prevent setting the age
+//     }
+//     target[prop] = value;
+//     return true; // indicate that the set was successful
+//   }
+// });
+
+// p1Proxy.age = -10;
+
+// console.log(p1Proxy.age); // testing
+
+const user = {
+  name: "Yash",
+  age: 24,
+  aadharNumber: "1234-5678-9012",
+  panNumber: "ABCDE1234F",
+  password: "pass123",
 };
 
-const p1Proxy = new Proxy(p1, {
+const userProxy = new Proxy(user, {
   get(target, prop) {
-    if(prop in target) {
-      return target[prop];
-    } else {
-      return "Property does not exist";
+    if (prop === "aadharNumber") {
+      console.log("Sensitive data");
+      return "xxxx-xxxx-9012";
     }
+
+    return Reflect.get({ ...arguments });
   },
-  set(target, prop, value) {
-    if(prop === "age" && value < 0) {
-      console.log("Age cannot be negative");
-      return false; // prevent setting the age
-    }
-    target[prop] = value;
-    return true; // indicate that the set was successful
-  }
 });
 
-p1Proxy.age = -10;
-
-console.log(p1Proxy.age); // testing
+console.log(userProxy.aadharNumber)
